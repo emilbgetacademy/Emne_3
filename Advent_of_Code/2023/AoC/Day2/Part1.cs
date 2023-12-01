bf3ed4ea5c6a4cb46c68d5d2ace6e6c158b8dcab
Emil Bratt <emilb@getacademy.no>
2023-12-01 15:20:23 +0100

    typing

--- Advent_of_Code/2023/AoC/Day2/Part1.cs
+++ Advent_of_Code/2023/AoC/Day2/Part1.cs
@@ -2,11 +2,10 @@ namespace AoC.Day2;
 
 class Part1
 {
-    private static string _result = String.Empty;
     public static string Run(string[] puzzle_input)
     {
         Console.WriteLine("Running day 2 part 1");
 
-        return _result;
+        return String.Empty;
     }
 }
--- Advent_of_Code/2023/AoC/Day2/Part2.cs
+++ Advent_of_Code/2023/AoC/Day2/Part2.cs
@@ -2,11 +2,10 @@ namespace AoC.Day2;
 
 class Part2
 {
-    private static string _result = String.Empty;
     public static string Run(string[] puzzle_input)
     {
         Console.WriteLine("Running day 2 part 2");
 
-        return _result;
+        return String.Empty;
     }
 }
