b759a3c510940cbfb6b5f461a0cd2edc8ed5ed8c
Emil Bratt <emilb@getacademy.no>
2023-12-02 23:48:45 +0100

    AoC Day 2 part 2 is finish (Day 2 is completed)

--- Advent_of_Code/2023/AoC/Day2/Part2.cs
+++ Advent_of_Code/2023/AoC/Day2/Part2.cs
@@ -23,30 +23,38 @@ class Part2
 
     private static int CalculatePowerOfMinimumSetOfCubes(string game_record)
     {
-        // foreach (string set in game_record.Split(";"))
-        // {
-        //     foreach(string subset in set.Split(","))
-        //     {
-        //         // remove leading and trailing whitespace
-        //         string trimmed_subset = subset.Trim();
+        int max_red = 0;
+        int max_green = 0;
+        int max_blue = 0;
+        foreach (string set in game_record.Split(";"))
+        {
+            foreach(string subset in set.Split(","))
+            {
+                // remove leading and trailing whitespace
+                string trimmed_subset = subset.Trim();
 
-        //         bool is_numeric = int.TryParse(trimmed_subset.Split(" ")[0], out int cube_count);
-        //         if (!is_numeric) Environment.Exit(1);
+                bool is_numeric = int.TryParse(trimmed_subset.Split(" ")[0], out int cube_count);
+                if (!is_numeric) Environment.Exit(1);
 
-        //         string cube_color = trimmed_subset.Split(" ")[1];
+                string cube_color = trimmed_subset.Split(" ")[1];
+                switch (cube_color)
+                {
+                    case "red":
+                        if (cube_count > max_red) max_red = cube_count;
+                        break;
+                    case "green":
+                        if (cube_count > max_green) max_green = cube_count;
+                        break;
+                    case "blue":
+                        if (cube_count > max_blue) max_blue = cube_count;
+                        break;
+                    default:
+                        Environment.Exit(1);
+                        break;
+                }
+            }
+        }
 
-        //         bool not_possible = cube_color switch
-        //         {
-        //             "red" => cube_count > 12,
-        //             "green" => cube_count > 13,
-        //             "blue" => cube_count > 14,
-        //             _ => false,
-        //         };
-
-        //         if (not_possible) return false;
-        //     }
-        // }
-
-        return 0;
+        return max_red * max_green * max_blue;
     }
 }
