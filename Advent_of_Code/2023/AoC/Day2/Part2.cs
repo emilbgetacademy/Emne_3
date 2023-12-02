00bdbfb6a2cf52bce38c3e495f787499ed4389fe
Emil Bratt <emilb@getacademy.no>
2023-12-02 23:31:46 +0100

    AoC starting with Day 2 part 2, using code from part 1, not finish yet

--- Advent_of_Code/2023/AoC/Day2/Part2.cs
+++ Advent_of_Code/2023/AoC/Day2/Part2.cs
@@ -4,8 +4,49 @@ class Part2
 {
     public static string Run(string[] puzzle_input)
     {
-        Console.WriteLine("Running day 2 part 2");
+        int result = 0;
+        for (int i = 0; i < puzzle_input.Length; i++)
+        {
+            result += HandlePuzzleLine(puzzle_input[i]);
+        }
 
-        return String.Empty;
+        return result.ToString();
+    }
+
+    private static int HandlePuzzleLine(string line)
+    {
+        string[] colon_split = line.Split(":");
+        string game_record = colon_split[1];
+
+        return CalculatePowerOfMinimumSetOfCubes(game_record);
+    }
+
+    private static int CalculatePowerOfMinimumSetOfCubes(string game_record)
+    {
+        // foreach (string set in game_record.Split(";"))
+        // {
+        //     foreach(string subset in set.Split(","))
+        //     {
+        //         // remove leading and trailing whitespace
+        //         string trimmed_subset = subset.Trim();
+
+        //         bool is_numeric = int.TryParse(trimmed_subset.Split(" ")[0], out int cube_count);
+        //         if (!is_numeric) Environment.Exit(1);
+
+        //         string cube_color = trimmed_subset.Split(" ")[1];
+
+        //         bool not_possible = cube_color switch
+        //         {
+        //             "red" => cube_count > 12,
+        //             "green" => cube_count > 13,
+        //             "blue" => cube_count > 14,
+        //             _ => false,
+        //         };
+
+        //         if (not_possible) return false;
+        //     }
+        // }
+
+        return 0;
     }
 }
