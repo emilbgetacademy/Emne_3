d2f2b07ee3089598cf87753c94975f291d08c785
Emil Bratt <emilb@getacademy.no>
2023-12-07 15:34:31 +0100

    AoC Day 5 Part 1 complete

--- Advent_of_Code/2023/AoC/DayX/Part1.cs
+++ Advent_of_Code/2023/AoC/DayX/Part1.cs
@@ -2,6 +2,7 @@ namespace AoC.DayX;
 
 class Part1
 {
+    #pragma warning disable CS8602 // Dereference of a possibly null reference.
     private static string[]? _puzzle_input;
 
     public static string Run(string[] puzzle_input)
--- Advent_of_Code/2023/AoC/DayX/Part2.cs
+++ Advent_of_Code/2023/AoC/DayX/Part2.cs
@@ -2,6 +2,7 @@ namespace AoC.DayX;
 
 class Part2
 {
+    #pragma warning disable CS8602 // Dereference of a possibly null reference.
     private static string[]? _puzzle_input;
 
     public static string Run(string[] puzzle_input)
